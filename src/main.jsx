9250ef6e87eda82bacae52952491b0ffd1293323
Clement Mukhale <xenobyte012@gmail.com>
2026-04-30 00:38:06 +0200

    done

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
+import React from 'react'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.StrictMode>
+    <BrowserRouter basename="/sir-simulator">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 )
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+  plugins: [react(), tailwindcss()],
+  base: "/sir-simulator/", 
+});
